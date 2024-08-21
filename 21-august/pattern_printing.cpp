bf2bce737cb960feadc971f5444c1066ca7164c4
Prashant Pandey <prashantpandey76867@gmail.com>
2024-08-21 12:30:20 +0530

    fgv

---
+++ 21-august/pattern_printing.cpp
@@ -0,0 +1,100 @@
+// *****
+// *****
+// *****
+// *****
+// *****
+//
+//*
+//** 
+//***
+//****
+//***** 
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     for(int i = 1; i <= 5; i++){
+//         for (int j = 1; j <= 5; j++){
+//             cout <<"*";
+//         }
+//         cout << "\n";
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     for(int i = 1; i <= 5; i++){
+//         for (int j = 1; j <= i; j++){
+//             cout <<"*";
+//         }
+//         cout << "\n";
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     for(int i = 1; i <= 5; i++){
+//         for (int j = 5; j >= i; j--){
+//             cout <<"*";
+//         }
+//         cout << "\n";
+//     }
+// }
+
+// *****
+//  ****
+//   ***
+//    **
+//     *
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     for(int i = 1; i <= 5; i++)
+//     {
+//         for (int j = 1; j < i; j++)
+//         {
+//             cout <<" ";
+//         }
+//         for(int k = 5; k >= i; k--){
+//             cout << "*";
+//         }
+//         cout << "\n";
+//     }
+// }
+
+//    *
+//   **
+//  ***
+// ****
+//*****
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     for(int i = 1; i <= 5; i++)
+//     {
+//         for (int j = 5; j > i; j--)
+//         {
+//             cout <<" ";
+//         }
+//         for(int k = 1; k <= i; k++){
+//             cout << "*";
+//         }
+//         cout << "\n";
+//     }
+// }
+
+// W.A.P to print the below pattern; but accept the symbol from user
+// 0000*0000
+// 000***000
+// 00*****00
+// 0*******0
+// *********
