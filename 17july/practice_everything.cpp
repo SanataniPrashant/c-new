5d5ef73df491219f6f7f04c7422dde5efb405a8e
Prashant Pandey <prashantpandey76867@gmail.com>
2024-08-26 01:29:39 +0530

    que

---
+++ 17july/practice_everything.cpp
@@ -0,0 +1,116 @@
+// 1.
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     cout << "Good Morning, Welcome to programming" << "\n";
+//     return 0;
+// }
+
+// //2.
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     cout << "Enter the side of square" << "\n";
+//     int side = 0;
+//     cin >> side;
+//     int area = 0, perimeter = 0;
+//     area = side * side;
+//     perimeter = 4 * side;
+//     cout << " area of square = " << area << "\n";
+//     cout << " perimeter of square = " << perimeter << "\n";
+//     return 0;
+// }
+
+//3. Write a program to print Hello World on console, and ask user, Do you want to continue? Press 1 to continue, Press 0 to exit. Accept user choice.
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int choice = 0;
+//     do
+//     {
+//         cout << "Hello World" << "\n";
+//         cout << "Do you want to continue? Press 1 to continue, Press 0 to exit" << "\n";
+//         cin >> choice;
+//     } while (choice != 0);
+//     return 0;
+// }
+
+// 4. Write a program print following menu to user:
+//   Main Menu:
+//   Press 1 for Addition,
+//   Press 2 for Subtraction,
+//   Press 3 for Multiplication,
+//   Press 4 fot Division,
+//   Press 0 to exit
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int choice = 0;
+//     do
+//     {
+//         cout << "Main Menu:" << "\n";
+//         cout << "Press 1 for Addition," << "\n";
+//         cout << "Press 2 for Subtraction," << "\n";
+//         cout << "Press 3 for Multiplication," << "\n";
+//         cout << "Press 4 for Division," << "\n";
+//         cout << "Press 0 to exit" << "\n";
+//         cin >> choice;
+//         switch (choice)
+//         {
+//             case 1:
+//             {
+//                 cout << "Addition" <<"\n";
+//                 break;
+//             }
+//             case 2:
+//             {
+//                 cout << "Subtraction" << "\n";
+//                 break;
+//             }
+//             case 3:
+//             {
+//                 cout << "Multiplication" << "\n";
+//                 break;  
+//             }
+//             case 4:
+//             {
+//                 cout << "Division" << "\n";
+//                 break;
+//             }
+//             case 0:
+//             {
+//                 cout << "Exiting..." << "\n";
+//                 break;
+//             }
+//             default:
+//             {
+//                 cout << "Invalid choice. Please choose a valid option." << "\n";
+//                 break;
+//             }
+//         }
+//     } while (choice != 0);
+//     return 0;
+// }
+
+// 4. Write a program to accept number from user, until user press 0 to exit. Print the count of all the entered number exclude 0
+#include <iostream>
+using namespace std;
+int main()
+{
+    int count = 0;
+    int number = 0;
+    do
+    {
+        cout << "Enter the no." << "\n";
+        cin >> number;
+        count ++;
+    } while (number != 0);
+    cout << "Total no. of numbers entered excluding 0 is " << count - 1;
+    return 0;
+}
+    
+    
---
+++ 23-august/question.cpp
@@ -0,0 +1,73 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     cout << "Enter the no. of games played by the player" << "\n";
+//     int game_number = 0;
+//     cin >> game_number;
+//     if(game_number == 99)
+//     {
+//         cout << "congratulations" << '\n';
+//     }
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int withdrawal_amount = 500;
+//     int pin = 454;
+//     cout << "Enter the pin" << '\n';
+//     cin >> pin;
+//     if(pin == 454)
+//     {
+//         cout << "withdrawal successful";
+//     }
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int length = 0;
+//     int breadth = 0;
+//     cout << "Enter the length and breadth" << "\n";
+//     cin >> length ;
+//     cin >> breadth;
+//     if(length == breadth)
+//     {
+//         cout << "square" << "\n";
+//     }
+//     else {
+//         cout << "rectangle" << "\n";
+//     }
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int starting_input = 0;
+//     int range = 0;
+//     cin >> starting_input;
+//     cin >> range;
+//     bool flag = 0;
+//     for(int i = starting_input + 1; i < range ; i++)
+//     {
+        
+        
+//     }
+// }
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n = 0;
+    int ne = 0 ; 
+    cin >> n;
+    
+}
