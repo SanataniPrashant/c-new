2477ec8221d20a48ed296addd15c147cfe042773
Prashant Pandey <prashantpandey76867@gmail.com>
2024-09-21 10:20:58 +0530

    sk

--- ram/ram.cpp
+++ ram/ram.cpp
@@ -12,4 +12,22 @@ int main()
     int r = 0, bill = 0, actual_bill = 0;
     cout << "Enter the reading" << "\n";
     cin >> r;
+    if(r>=1 && r<=100){
+        bill = 3*r;
+    }
+    else if(r>100 && r<=200){
+        bill = 5*r;
+    }
+    else if(r>200 && r<=500){
+        bill = 7*r;
+    }
+    else if(r>500){
+        bill = 10*r;
+    }
+    else{
+        cout << "Invalid choice" << "\n";
+    }
+    actual_bill = bill + (bill * 0.2);
+    cout << "Actual bill is " << actual_bill << "\n";
+    return 0;
 }
