1d96f75a95d387a8361995b5c2cca706b430a808
Prashant Pandey <prashantpandey76867@gmail.com>
2024-11-06 21:28:47 +0530

    fsjfkjkjfkjsjkkjssjnk

---
+++ array/introduction.cpp
@@ -0,0 +1,18 @@
+// ==>>  ARRAY : 
+// =>  It is a collection of similar kind of data element.
+// =>  The indexing of array starts from 0.
+// =>  The memory allocation of array is contiguous / sequentially. The data stored in sequence.
+// =>  It is used to store multiple value.
+// =>  With the help of array we can search the data quickly .\
+// =>  There are to types of array :
+// 1.  Numetric Array : 
+//                      1. 1 Diamentional Array
+//                      2. 2 Diamentional Array
+//                      3. 3 Diamentional Array
+// {
+//     It is less secure because It can be accessed through its index value which is univesally known that it satarts from zero and sequentially inceases.
+// }
+// 2.  Associative array
+// {
+//     It is more secure because It is accessed through its key(It is unique and random and cannot reapeate).
+// }
