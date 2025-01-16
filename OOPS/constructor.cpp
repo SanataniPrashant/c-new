91e51e5925e170ed9f85d1332662420e9e695639
Prashant Pandey <prashantpandey76867@gmail.com>
2025-01-16 23:47:06 +0530

    h

--- OOPS/constructor.cpp
+++ OOPS/constructor.cpp
@@ -260,3 +260,314 @@
 // }
 
 
+// #include<iostream>
+// #include<string>
+// using namespace std;
+// class vehicle{
+//     string vehicleno , color;
+//     public:
+//     vehicle(string vehicleno, string color){
+//         this->vehicleno = vehicleno;
+//         this->color = color;
+//     }
+//     virtual void info(){
+//         cout << "Vehicle Number: "<< vehicleno <<"\n" ;
+//         cout << "Vehicle Color: "<< color <<"\n" ;
+//     }
+// };
+// class Twowheeler : public vehicle{
+//     public:
+//     Twowheeler(string vehicleno, string color):vehicle(vehicleno,color){
+//     }
+//     void info(){
+//         vehicle::info();
+//         cout << "class Twowheeler" << "\n";
+//     }
+// };
+// class fourwheeler : public vehicle{
+//     public:
+//     fourwheeler(string vehicleno, string color):vehicle(vehicleno,color){
+//     }
+//     void info(){
+//         vehicle::info();
+//         cout << "class fourwheeler" << "\n";
+//     }
+// };
+// int main(){
+// //    Twowheeler bike("ghj","dfgh");
+// //    bike.info();
+// //    fourwheeler car("vnb55","dhdhd");
+// //    car.info();
+// vehicle *v ;
+// Twowheeler obj("ghj","dfgh");
+// fourwheeler obj1("vnb55","dhdhd");
+// v = &obj;
+// v->info();
+// v = &obj1;
+// v->info();
+
+//     return 0;
+// }
+
+
+//Write a program to find Area and Circumference of different shapes using polymorphism concept. in cpp
+
+// #include <iostream>
+// using namespace std;
+
+// // Base class Shape
+// class Shape {
+// public:
+//     // Virtual function to calculate area
+//     virtual double area() const = 0; // Pure virtual function
+
+//     // Virtual function to calculate circumference
+//     virtual double circumference() const = 0; // Pure virtual function
+
+//     // Virtual destructor
+//     virtual ~Shape() {}
+// };
+
+// // Derived class Circle
+// class Circle : public Shape {
+// private:
+//     double radius;
+
+// public:
+//     Circle(double r) : radius(r) {}
+
+//     // Override area() for Circle
+//     double area() const override {
+//         return 3.14159 * radius * radius; // πr²
+//     }
+
+//     // Override circumference() for Circle
+//     double circumference() const override {
+//         return 2 * 3.14159 * radius; // 2πr
+//     }
+// };
+
+// // Derived class Rectangle
+// class Rectangle : public Shape {
+// private:
+//     double length;
+//     double width;
+
+// public:
+//     Rectangle(double l, double w) : length(l), width(w) {}
+
+//     // Override area() for Rectangle
+//     double area() const override {
+//         return length * width; // l * w
+//     }
+
+//     // Override circumference() for Rectangle
+//     double circumference() const override {
+//         return 2 * (length + width); // 2(l + w)
+//     }
+// };
+
+// int main() {
+//     // Create objects of Circle and Rectangle
+//     Circle circle(5.0);
+//     Rectangle rectangle(4.0, 6.0);
+
+//     // Use base class pointers to achieve polymorphism
+//     Shape* shape1 = &circle;
+//     Shape* shape2 = &rectangle;
+
+//     // Calculate and display area and circumference
+//     cout << "Circle:" << endl;
+//     cout << "Area: " << shape1->area() << endl;
+//     cout << "Circumference: " << shape1->circumference() << endl;
+
+//     cout << "\nRectangle:" << endl;
+//     cout << "Area: " << shape2->area() << endl;
+//     cout << "Circumference: " << shape2->circumference() << endl;
+
+//     return 0;
+// }
+
+// Write a program using static method & static variable. in cpp
+// #include <iostream>
+// using namespace std;
+
+// class s{
+//     static int a,b;
+//     public:
+//     static void sum(int c, int d)
+//     {
+//         a=c;
+//         b=d;
+//         cout<<"sum of a and b is "<<a+b<<endl;
+//     }
+//     static void display()
+//     {
+//         cout<<"value of a is "<<a<<endl;
+//         cout<<"value of b is "<<b<<endl;
+//     }
+// };
+// int s::a=0;
+// int s::b=0;
+// int main()
+// {
+//     s::sum(10,20);
+//     s::display();
+//     return 0;
+// }    
+
+
+
+//Write a program to create diamond problem in inheritance .in cpp
+// #include <iostream>
+// using namespace std;
+// class A{
+//     public:
+//     void display()
+//     {
+//         cout<<"A"<<endl;
+//     }
+// };
+// class B:public A{
+//     public:
+//     void displayB()
+//     {
+//         cout<<"B"<<endl;
+//     }
+// };
+// class C:public A{
+//     public:
+//     void displayC()
+//     {
+//         cout<<"C"<<endl;
+//     }
+// };
+// class D:public B , public C{
+//     public:
+//     void displayD()
+//     {
+//         cout<<"D"<<endl;
+//     }
+// };
+// int main()
+// {
+//     D obj;
+//     obj.display();// ambiguidity
+//     obj.displayB();
+//     obj.displayC();
+//     obj.displayD();
+//     return 0;
+// }
+
+
+//Write a program to handle the exception of double and char value. in cpp
+
+// #include <iostream>
+// #include <stdexcept> // For std::invalid_argument
+
+// // Function to check if a double value is valid
+// void checkDouble(double value) {
+//     if (value < 0) {
+//         throw std::invalid_argument("Double value cannot be negative!");
+//     }
+//     std::cout << "Double value is valid: " << value << std::endl;
+// }
+
+// // Function to check if a char value is valid
+// void checkChar(char value) {
+//     if (!isalpha(value)) {
+//         throw std::invalid_argument("Char value must be an alphabet!");
+//     }
+//     std::cout << "Char value is valid: " << value << std::endl;
+// }
+
+// int main() {
+//     try {
+//         // Test double value
+//         double doubleValue;
+//         std::cout << "Enter a double value: ";
+//         std::cin >> doubleValue;
+//         checkDouble(doubleValue);
+
+//         // Test char value
+//         char charValue;
+//         std::cout << "Enter a char value: ";
+//         std::cin >> charValue;
+//         checkChar(charValue);
+//     }
+//     catch (const std::invalid_argument& e) {
+//         std::cerr << "Exception caught: " << e.what() << std::endl;
+//     }
+//     catch (...) {
+//         std::cerr << "An unknown exception occurred!" << std::endl;
+//     }
+
+//     return 0;
+// }
+
+//W.a.cpp program for constructor overloading
+
+// #include <iostream>
+// #include <string>
+
+// class Student {
+// private:
+//     std::string name;
+//     int age;
+//     double grade;
+
+// public:
+//     // Default Constructor
+//     Student() {
+//         name = "Unknown";
+//         age = 0;
+//         grade = 0.0;
+//         std::cout << "Default constructor called!" << std::endl;
+//     }
+
+//     // Parameterized Constructor (with name only)
+//     Student(std::string n) {
+//         name = n;
+//         age = 0;
+//         grade = 0.0;
+//         std::cout << "Parameterized constructor (name only) called!" << std::endl;
+//     }
+
+//     // Parameterized Constructor (with name and age)
+//     Student(std::string n, int a) {
+//         name = n;
+//         age = a;
+//         grade = 0.0;
+//         std::cout << "Parameterized constructor (name and age) called!" << std::endl;
+//     }
+
+//     // Parameterized Constructor (with name, age, and grade)
+//     Student(std::string n, int a, double g) {
+//         name = n;
+//         age = a;
+//         grade = g;
+//         std::cout << "Parameterized constructor (name, age, and grade) called!" << std::endl;
+//     }
+
+//     // Display student details
+//     void display() {
+//         std::cout << "Name: " << name << ", Age: " << age << ", Grade: " << grade << std::endl;
+//     }
+// };
+
+// int main() {
+//     // Creating objects using different constructors
+//     Student s1; // Default constructor
+//     s1.display();
+
+//     Student s2("Alice"); // Constructor with name only
+//     s2.display();
+
+//     Student s3("Bob", 20); // Constructor with name and age
+//     s3.display();
+
+//     Student s4("Charlie", 22, 95.5); // Constructor with name, age, and grade
+//     s4.display();
+
+//     return 0;
+// }
